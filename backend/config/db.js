3694572fa7c1d52f4dd5bdb86dbde1732167c6f7
Bharath <83769872+BharathKumar-c@users.noreply.github.com>
2025-03-01 13:41:34 +0530

    Remove deprecated mongoose connected option

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,8 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
